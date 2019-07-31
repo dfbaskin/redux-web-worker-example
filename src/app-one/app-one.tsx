8dd899082b4308007e02f51972329ece8fb3b49a
Dave Baskin <dfbaskin@gmail.com>
2019-07-31 10:58:00 -0400

    Adding initial application files.

---
+++ src/app-one/app-one.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Hello } from "../ui-library/components/Hello";
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
