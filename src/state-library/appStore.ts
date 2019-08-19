dabf89e23cdc4e78859b54333f3e9983c58653fc
Dave Baskin <dfbaskin@gmail.com>
2019-08-19 14:44:23 -0400

    Adding redux thunk.

--- src/state-library/appStore.ts
+++ src/state-library/appStore.ts
@@ -1,6 +1,7 @@
 import { reducer } from "./actions";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export { store };
